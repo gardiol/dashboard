4b0a017676d0084900a836449ab9f41af1a1b36b
Willy Gardiol <willy@gardiol.org>
2024-04-09 15:06:55 +0200

    small visual improvement in ping monitor

--- monitor/templates/ping.sh
+++ monitor/templates/ping.sh
@@ -5,13 +5,7 @@ echo "<div><div class=''>Devices:</div>"
 N=0
 while [ ${N} -lt ${PING_NUM} ]
 do
-	if [ ${PING_STATUS[${N}]} -eq 0 ]
-	then
-		value="-up-";
-	else
-		value="down";
-	fi
-	echo "<div><span class='${PING_CLASS[${N}]}'>${PING_HOSTS[${N}]}: </span><span class='${PING_CLASS[${N}]}'>${value}</span></div>"
+	echo "<div><span class='${PING_CLASS[${N}]}'>${PING_HOSTS[${N}]}</span></div>"
 	N=$(( N+1 ))
 done
 
